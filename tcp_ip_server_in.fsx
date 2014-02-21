8660c655fa650439f28dc6ff091f4e1864a301ff
Gino Ungureanu <gino.ungureanu@teamnet.ro>
2014-02-21 13:46:00 +0200

    Fixed names of tcp ip servers

--- tcp_ip_server_in.fsx
+++ tcp_ip_server_in.fsx
@@ -3,32 +3,27 @@ open System.Threading
 open System.Net
 open System.Net.Sockets
 
+let read (clientStream:Stream) = 
+    use streamReader = new StreamReader(clientStream)
+    let rec readline reader = async {
+        let! line = streamReader.ReadLineAsync() |> Async.AwaitTask
+        match line with
+        | null -> ()
+        | _ -> 
+            do printfn "%s" line; 
+            return! readline reader
+    }
+    readline streamReader |> Async.RunSynchronously
 
-let rec mirror (reader:TextReader) (writer:StreamWriter) = async {
-    let! line = reader.ReadLineAsync() |> Async.AwaitTask
-    match line with
-    | null -> ()
-    | _ ->
-        let task = writer.WriteLineAsync(line)
-        do! task.ContinueWith(fun t -> ()) |> Async.AwaitTask
-        return! mirror reader writer
-}
-
-let writer (client:TcpClient) =
-    let newWriter = new StreamWriter(client.GetStream())
-    newWriter.AutoFlush <- true
-    newWriter
+let stream (client:TcpClient) = client.GetStream()
 
 let tcp_ip_server (sourceip,sourceport) =
     let server = new TcpListener(IPAddress.Parse(sourceip),sourceport)
     server.Start()
-    while true do
-        let client = server.AcceptTcpClient()
-        let t = new Thread(ThreadStart(fun _ ->
-            try
-                mirror System.Console.In <| writer client |> Async.RunSynchronously 
-            with |_ -> client.Close())
-        , IsBackground = true)
-        t.Start()
+        
+    let client = server.AcceptTcpClient()
+    try
+        stream (client) |> read 
+    with |_ -> client.Close()
 
 tcp_ip_server (fsi.CommandLineArgs.[1], int fsi.CommandLineArgs.[2])
--- tcp_ip_server_out.fsx
+++ tcp_ip_server_out.fsx
@@ -3,27 +3,32 @@ open System.Threading
 open System.Net
 open System.Net.Sockets
 
-let read (clientStream:Stream) = 
-    use streamReader = new StreamReader(clientStream)
-    let rec readline reader = async {
-        let! line = streamReader.ReadLineAsync() |> Async.AwaitTask
-        match line with
-        | null -> ()
-        | _ -> 
-            do printfn "%s" line; 
-            return! readline reader
-    }
-    readline streamReader |> Async.RunSynchronously
 
-let stream (client:TcpClient) = client.GetStream()
+let rec mirror (reader:TextReader) (writer:StreamWriter) = async {
+    let! line = reader.ReadLineAsync() |> Async.AwaitTask
+    match line with
+    | null -> ()
+    | _ ->
+        let task = writer.WriteLineAsync(line)
+        do! task.ContinueWith(fun t -> ()) |> Async.AwaitTask
+        return! mirror reader writer
+}
+
+let writer (client:TcpClient) =
+    let newWriter = new StreamWriter(client.GetStream())
+    newWriter.AutoFlush <- true
+    newWriter
 
 let tcp_ip_server (sourceip,sourceport) =
     let server = new TcpListener(IPAddress.Parse(sourceip),sourceport)
     server.Start()
-        
-    let client = server.AcceptTcpClient()
-    try
-        stream (client) |> read 
-    with |_ -> client.Close()
+    while true do
+        let client = server.AcceptTcpClient()
+        let t = new Thread(ThreadStart(fun _ ->
+            try
+                mirror System.Console.In <| writer client |> Async.RunSynchronously 
+            with |_ -> client.Close())
+        , IsBackground = true)
+        t.Start()
 
 tcp_ip_server (fsi.CommandLineArgs.[1], int fsi.CommandLineArgs.[2])
