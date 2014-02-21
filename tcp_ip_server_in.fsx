b515e59e336d61187417a36fc5335f8cf877d506
Gino Ungureanu <gino.ungureanu@teamnet.ro>
2014-02-21 12:35:28 +0200

    Created tcp server for sending input to connected clients

---
+++ tcp_ip_server_in.fsx
@@ -0,0 +1,34 @@
+open System.IO
+open System.Threading
+open System.Net
+open System.Net.Sockets
+
+
+let rec mirror (reader:TextReader) (writer:StreamWriter) = async {
+    let! line = reader.ReadLineAsync() |> Async.AwaitTask
+    match line with
+    | null -> ()
+    | _ ->
+        let task = writer.WriteLineAsync(line)
+        do! task.ContinueWith(fun t -> ()) |> Async.AwaitTask
+        return! mirror reader writer
+}
+
+let writer (client:TcpClient) =
+    let newWriter = new StreamWriter(client.GetStream())
+    newWriter.AutoFlush <- true
+    newWriter
+
+let tcp_ip_server (sourceip,sourceport) =
+    let server = new TcpListener(IPAddress.Parse(sourceip),sourceport)
+    server.Start()
+    while true do
+        let client = server.AcceptTcpClient()
+        let t = new Thread(ThreadStart(fun _ ->
+            try
+                mirror System.Console.In <| writer client |> Async.RunSynchronously 
+            with |_ -> client.Close())
+        , IsBackground = true)
+        t.Start()
+
+tcp_ip_server (fsi.CommandLineArgs.[1], int fsi.CommandLineArgs.[2])
