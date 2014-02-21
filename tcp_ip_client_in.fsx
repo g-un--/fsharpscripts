37073499b9cc899f94274dee34da42f4508b72d1
Gino Ungureanu <gino.ungureanu@teamnet.ro>
2014-02-21 13:44:33 +0200

    Created in/out tcp ip clients

---
+++ tcp_ip_client_in.fsx
@@ -0,0 +1,26 @@
+open System.IO
+open System.Threading
+open System.Net
+open System.Net.Sockets
+
+let read (clientStream:Stream) =
+    use streamReader = new StreamReader(clientStream)
+    let rec readline reader = async {
+        let! line = streamReader.ReadLineAsync() |> Async.AwaitTask
+        match line with
+        | null -> ()
+        | _ ->
+            do printfn "%s" line;
+            return! readline reader
+    }
+    readline streamReader |> Async.RunSynchronously
+
+let stream (client:TcpClient) = client.GetStream()
+
+let tcp_ip_client (targetip, targetport) =
+    let client = new TcpClient(targetip, targetport)
+    try
+        stream (client) |> read
+    with |_ -> client.Close()
+
+tcp_ip_client (fsi.CommandLineArgs.[1], int fsi.CommandLineArgs.[2])
